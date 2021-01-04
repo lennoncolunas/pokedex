b09eac9ca2fd52ff05ea804a22e5cd97bf81cc7c
lennoncolunas <lennon.colunas@gmail.com>
2021-01-04 16:12:17 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="pokemon" v-for="(poke, index) in pokemons" :key="index" >
+      <Pokemon :num=index+1 :name=poke.name :url=poke.url />
+      <button class="button" @click="salvarPokemon(index+1, poke.name, poke.url)" > Salvar</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Pokemon from './components/Pokemon'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      pokemons:[]
+    }
+  },
   components: {
-    HelloWorld
+    Pokemon
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("PEGOU DA LISTA DE POKEMONS");
+      this.pokemons = res.data.results;
+     // console.log(this.pokemons);
+    });
+      
+  },
+  methods:{
+    salvarPokemon: function(index, nome, url){
+
+/*
+      const options = {
+      method: 'post',
+      url: 'https://pokemons-f24aa-default-rtdb.firebaseio.com/pokemons',
+      xsrfCookieName: 'XSRF-TOKEN',
+      xsrfHeaderName: 'X-XSRF-TOKEN',
+    };
+
+    // send the request
+    axios(options);  */
+
+      console.log("ID:" + index + " NOME: " + nome + " URL: " + url);
+      axios.post('https://pokemons-f24aa-default-rtdb.firebaseio.com/pokemons', {id: 'index', nome: 'nome', url: 'url'});
+    }
   }
 }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="pokemon">
+      <h1>{{num}} - {{name | upper}}</h1>
+      <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+export default {
+props:{
+    num: Number,
+    name: String,
+    url: String
+},
+filters:{
+    upper: function(valor){
+        var newValor = valor[0].toUpperCase() + valor.slice(1);
+        return newValor;
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
